d23aaecf1992451e99ab879ce8627b287451eeb6
rjtparashar <parasarrajat@gmail.com>
2022-01-09 16:34:41 +0530

    till while

---
+++ file1.js
@@ -0,0 +1,56 @@
+let a = 2
+console.log("hi rajat",a);
+
+//string
+// let str = "piyush         "
+// console.log('str: ', str[0]);
+// console.log("str.length",str.length);
+
+//trim 
+//extra spaces 
+// let va = "    rajat   "
+// console.log(va.length);
+// // va.trim().length
+// console.log(va.length);
+// va = va.trim()
+// console.log('va: ', va.length);
+
+
+//slice 
+// let yu = "rajat"
+// console.log('yu: ', yu.slice(0,2));
+
+
+//stringToNumber && numberToString
+// let nu = 25
+// console.log('nu: ', +"nu");
+// nu = nu + ""
+// console.log("nu",typeof nu);
+
+// let stri = "4"
+// console.log('stri: ', typeof stri);
+// stri = +stri
+// console.log('stri: ', stri);
+// console.log('stri: ', typeof stri);
+
+
+// let simple = 34
+// console.log("simple",typeof simple);
+// let changedSimple = String(simple)
+// console.log("changedSimple",typeof changedSimple);
+// let simpleNew = Number(changedSimple)
+// console.log('simpleNew: ',typeof simpleNew);
+
+//concatenation
+// let string1 = "12";
+// let string2 = "19";
+// // let newSrring = 
+// console.log(string1 + string2); //output will be 1219
+// console.log(+string1 + +string2); //output will be 31
+
+//templateString 
+// let num = 13 ;
+// let name = "khubi"
+// console.log(typeof name);
+// console.log(`my name is ${name} and age is ${num}`);
+
---
+++ file2.js
@@ -0,0 +1,52 @@
+console.log("1234567890");
+// let yiy;
+// console.log('yiy: ', yiy); //it will be undefines
+// // const iy
+// // console.log('iy: ', iy); //it will give an error 
+// var yi
+// console.log("yi:",yiy);  //it will be undefines
+
+// let yui = null;
+// console.log('yui: ', yui,typeof yui);
+// console.log(Number.MAX_SAFE_INTEGER);
+// let bh = 123n
+// console.log('bh: ', bh); ///bigInt
+// bh = BigInt(12)
+// console.log('bh: ', bh);
+
+// let you = undefined;
+// if(you){
+//     console.log("IYOU");
+// }else{
+//     console.log("asdfg");
+// }
+//  a= 13
+// let b ="rjt" 
+// console.log(a+b);
+
+
+//ternaryOperator
+// let gameWin = 19
+// let input = +prompt("guess a number")
+// console.log('input: ', input);
+// let message = input>=20 ? "TooHigh" : input<=17 ? "tooLow" : input===gameWin ? "you Won" : input==18 ? "nearByOneup" : "nearByOneDown" 
+// console.log('message: ', message);
+
+//loop
+// (function test(n,m) {
+//     // body...
+//     if (n<=m) {
+//         console.log(n);
+//         n++;
+//         test(n,m);
+//     }
+// })(1,10);    
+// console.log(i);
+let i = 1    
+k = 0
+while(i<11){
+     console.log(i,"i");
+    k = k + i
+    i++ 
+    console.log(k)
+}
